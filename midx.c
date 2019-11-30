7401d7da83d4b79b06674f4764de011c102911d6
Thomas Gummerer <t.gummerer@gmail.com>
2019-11-30 18:30:28 +0000

    midx: make constant unsigned

--- midx.c
+++ midx.c
@@ -10,7 +10,7 @@
 #include "progress.h"
 #include "trace2.h"
 
-#define MIDX_SIGNATURE 0x4d494458 /* "MIDX" */
+#define MIDX_SIGNATURE 0x4d494458u /* "MIDX" */
 #define MIDX_VERSION 1
 #define MIDX_BYTE_FILE_VERSION 4
 #define MIDX_BYTE_HASH_VERSION 5
